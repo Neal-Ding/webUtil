249b23f98b726ed7461a95173c9b9f948f721132
jfw10973 <jfw10973@gmail.com>
2014-12-29 19:11:34 +0800

    add img2dataurl

--- img2base64/client/js/page.js
+++ img2base64/client/js/page.js
@@ -1,39 +1,36 @@
 window.onload = function () {
 	var imgArea = document.querySelector('.img-area');
-	var processBar = document.querySelector('.process-bar');
+	var imgAreaTip = imgArea.children[0];
 	var codeArea = document.querySelector('.code-area');
 	var result = {};
 
 	function dragInit () {
-		getDragImg();
-	}
-
-	function getDragImg () {
-		imgArea.addEventListener('drag', handleDrag, false);
-		imgArea.addEventListener('dragover', handleDragOver, false);
+		imgArea.addEventListener('dragenter', handleDragEnter, false);
+		imgArea.addEventListener('dragover', handleDragEnter, false);
+		imgArea.addEventListener('dragleave', handleDragLeave, false);
 		imgArea.addEventListener('drop', handleDrop, false);
 	}
-
-	function handleDragOver (e) {
+	function handleDragEnter (e) {
 		e.preventDefault();
+		imgAreaTip.innerText = "释放生成DataURL";
 	}
-	function handleDrag () {
-
+	function handleDragLeave () {
+		imgAreaTip.innerText = "将图片拖拽至此";
 	}
 	function handleDrop (e) {
-		e.stopPropagation();
 		e.preventDefault();
+		imgAreaTip.innerText = "处理中...";
 		result = {};
 		var files = Array.prototype.slice.call(e.dataTransfer.files);
 		var reader = new FileReader();
 		files.forEach(function (t) {
 			reader.onload = function(event) {
 				result[t.name] = event.target.result;
-				codeArea.innerHTML = event.target.result;
-			}
+				codeArea.innerText = event.target.result;
+				imgAreaTip.innerText = "将图片拖拽至此";
+			};
 			reader.readAsDataURL(t);
-		})
+		});
 	}
-
 	dragInit();
-}
+};
