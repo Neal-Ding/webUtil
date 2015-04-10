51543794127410d338f6f016979ee4717af54e48
jfw10973 <jfw10973@gmail.com>
2015-04-10 15:59:00 +0800

    完全抽离DOM，集中在init里做，抽离模板，加入渲染函数

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 //全局公用逻辑
 var G = {
+    init: function () {
+        var w = this;
+        w.tipMsg = document.querySelector('.G-tipMsg');
+    },
     ns: function (str, overWritten) {
         var arr = str.split("."),
             w = window;
@@ -15,26 +19,49 @@ var G = {
             w = w[arr[i]];
         }
     },
+    //兼容模板内容，单节点和多节点
+    //todo
+    //返回元素是否必须用tempDOM中转？参考下handlebars和underscore
+    //返回collection还是nodelist 比较好？目前是collection
+    //目前是根据节点数量返回单一节点或者是collection, 是否有必要？
+    setTemplate: function (input) {
+        var tempDOM = document.createElement('div'),
+            template;
+
+        if(typeof(input) === 'string'){
+            tempDOM.innerHTML = input;
+        }
+        else if(input !== undefined){
+            input = (input.nodeType === 1) ? [input] : Array.prototype.slice.call(input);
+            input.forEach(function (t) {
+                tempDOM.innerHTML += t.innerHTML;
+            });
+        }
+
+        template = (tempDOM.children.length > 1) ? tempDOM.children : tempDOM.children[0];
+        return template;
+    },
     share: {
         showPassTip: function (text) {
-            var t = this;
-            t._showTip(text, 0);
+            var w = this;
+            w._showTip(text, 0);
         },
         showWarnTip: function (text) {
-            var t = this;
-            t._showTip(text, 1);
+            var w = this;
+            w._showTip(text, 1);
         },
         showErrorTip: function (text) {
-            var t = this;
-            t._showTip(text, 2);
+            var w = this;
+            w._showTip(text, 2);
         },
         _showTip: function (text, status) {
-            var tipMsg = document.querySelector('.G-tipMsg'),
+            var w = this,
+                tipMsg = w.tipMsg,
+                tipTemplate = '<div class="G-tipMsg"></div>',
                 statusArr = ['pass', 'warn', 'error'];
 
             if(!tipMsg){
-                tipMsg = document.createElement("div");
-                tipMsg.className = 'G-tipMsg';
+                tipMsg = G.setTemplate(tipTemplate);
                 document.body.appendChild(tipMsg);
             }
             tipMsg.innerText = text || '';
@@ -51,8 +78,8 @@ var G = {
 
 //处理拖拽逻辑
 G.drag = {
-    init: function (req, res) {
-        var t = this;
+    init: function (request, response) {
+        var w = this;
 
         ZeroClipboard.config({
             moviePath: 'js/ZeroClipboard.swf',
@@ -61,17 +88,19 @@ G.drag = {
             trustedDomains: ['*']
         });
 
-        t.req = req;
-        t.res = res;
-        t.eventHandle();
+        w.src = request.source;
+        w.tip = request.source.children[0];
+        w.res = response;
+        w.tpl = request.template;
+        w.eventHandle();
     },
     eventHandle: function () {
-        var t = this;
+        var w = this;
 
-        t.req.addEventListener('dragenter', t.dragEnterHandle, false);
-        t.req.addEventListener('dragover', t.dragEnterHandle, false);
-        t.req.addEventListener('dragleave', t.dragLeaveHandle, false);
-        t.req.addEventListener('drop', t.dropHandle, false);
+        w.src.addEventListener('dragenter', w.dragEnterHandle, false);
+        w.src.addEventListener('dragover', w.dragEnterHandle, false);
+        w.src.addEventListener('dragleave', w.dragLeaveHandle, false);
+        w.src.addEventListener('drop', w.dropHandle, false);
     },
     dragEnterHandle: function (e) {
         var w = G.drag;
@@ -96,31 +125,71 @@ G.drag = {
         w.setAreaStatus(2);
         files.forEach(function (t) {
             if(t.type.indexOf('image') != -1){
-                var item = document.createElement('li');
-                item.innerHTML =
-                '<img alt="img" class="img">' +
-                '<button class="copy">复制编码</button>' +
-                '<span class="status"></span>';
-                docfrag.appendChild(item);
-
+                var item = G.setTemplate(w.tpl);
                 G.reader.init(t, item);
+                docfrag.appendChild(item);
             }
         });
         w.res.appendChild(docfrag);
-        w.setClipBoard(document.querySelectorAll('.copy'));
         w.setAreaStatus(0);
     },
     setAreaStatus: function (status) {
         var tipArr = ['将图片拖拽至此', '释放生成DataURL', '处理中...', '只接受图片类文件'],
             w = G.drag;
 
-        w.req.children[0].innerText = tipArr[status];
+        w.tip.innerText = tipArr[status];
+    }
+};
+
+//读取数据逻辑
+G.reader = {
+    init: function (file, itemNode) {
+        var w = this,
+            reader = new FileReader();
+
+        w.file = file;
+        w.itemNode = itemNode;
+
+        reader.imgNode = w.itemNode.querySelector('.img');
+        reader.copyNode = w.itemNode.querySelector('.copy');
+        reader.statusNode = w.itemNode.querySelector('.status');
+
+        w.eventHandle(reader);
+    },
+    eventHandle: function (reader) {
+        var w = this;
+
+        reader.onloadstart = w.onloadStart;
+        reader.onload = w.onload;
+        reader.readAsDataURL(w.file);
+    },
+    onloadStart: function () {
+        var t = this,
+            w = G.reader;
+
+        w.setStatus(t.statusNode, t.readyState);
+    },
+    onload: function () {
+        var t = this,
+            w = G.reader;
+
+        t.imgNode.src = t.result;
+        w.setStatus(t.statusNode, t.readyState);
+        //todo
+        //节点委托
+        w.setClipBoard(t.copyNode);
+    },
+    setStatus: function (node, status) {
+        if(status == 2){
+            node.style.height = 0;
+        }
     },
     setClipBoard: function (btn) {
         var copy = new ZeroClipboard(btn);
+
         copy.on('load', function(client) {
             client.on('datarequested', function(client) {
-                client.setText(this.previousSibling.src);
+                client.setText(this.previousElementSibling.src);
             });
             client.on('complete', function() {
                 G.share.showPassTip('复制成功');
@@ -132,50 +201,15 @@ G.drag = {
     }
 };
 
-//读取数据逻辑
-G.reader = {
-    init: function (file, itemNode) {
-        var t = this;
-
-        t.itemNode = itemNode;
-        t.file = file;
-
-        t.eventHandle();
-    },
-    eventHandle: function () {
-        var t = this,
-            reader = new FileReader();
-
-        reader.itemNode = t.itemNode;
-        reader.onloadstart = t.onloadStart;
-        reader.onload = t.onload;
-        reader.readAsDataURL(t.file);
-    },
-    onloadStart: function () {
-        var t = this,
-            w = G.reader;
-
-        w.setStatus(t.itemNode, 1);
-    },
-    onload: function () {
-        var t = this,
-            w = G.reader;
-
-        t.itemNode.querySelector('.img').src = t.result;
-        w.setStatus(t.itemNode, 2);
-    },
-    setStatus: function (obj, status) {
-        if(status == 2){
-            obj.querySelector('.status').style.height = 0;
-        }
-    }
-};
-
 window.onload = function () {
     // todo
     // base64切割输出带进度，保证大图不卡，BUT 切割后无法组装回DataURL，目前无解
-    var request = document.querySelector('.img-area'),
+    var request = {
+            source: document.querySelector('.img-area'),
+            template: document.querySelectorAll('.code-list-template')
+        },
         response = document.querySelector('.code-list');
 
+    G.init();
     G.drag.init(request, response);
 };
